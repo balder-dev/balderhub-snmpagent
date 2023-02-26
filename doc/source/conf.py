799315e05283586a2f9fce3fa07e7112fc499207
matosys <max@matosys.de>
2023-02-26 11:18:58 +0100

    docs(docs/*): add basic documentation structure

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -3,6 +3,11 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('../../src'))
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
@@ -13,15 +18,19 @@ author = 'Max Stahlschmidt'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['sphinx.ext.autodoc']
 
 templates_path = ['_templates']
 exclude_patterns = []
-
-
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+# -- Autodoc settings --------------------------------------------------------
+
+autodoc_default_options = {
+    'show-inheritance': True
+}
---
+++ doc/source/features.rst
@@ -0,0 +1,40 @@
+Features
+********
+
+
+Scenario Features
+=================
+
+SnmpManagerDevice
+-----------------
+
+.. autoclass:: balderhub.snmpagent.lib.features.HasSnmpSystemGroupFeature
+    :members:
+
+.. autoclass:: balderhub.snmpagent.lib.features.SnmpSystemConfig
+    :members:
+
+SnmpAgentDevices
+----------------
+
+.. autoclass:: balderhub.snmpagent.lib.features.SendSnmpGetRequestCommandFeature
+    :members:
+
+.. autoclass:: balderhub.snmpagent.lib.features.SendSnmpSetRequestCommandFeature
+    :members:
+
+
+PySNMP setup Features
+=====================
+
+SnmpAgentDevices
+----------------
+
+This section holds features that can directly be used in your setup.
+
+.. autoclass:: balderhub.snmpagent.lib.setup_features.SendSnmpGetRequestPysnmpFeature
+    :members:
+
+.. autoclass:: balderhub.snmpagent.lib.setup_features.SendSnmpSetRequestPysnmpFeature
+    :members:
+
